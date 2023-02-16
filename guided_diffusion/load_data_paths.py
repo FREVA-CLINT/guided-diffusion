1e232bea9954c3f9b4e2e1507e048c2d91d44d12
Johannes Meuer <meuer@dkrz.de>
2023-02-16 18:18:03 +0100

    fix

--- guided_diffusion/load_data_paths.py
+++ guided_diffusion/load_data_paths.py
@@ -8,7 +8,7 @@ import freva
 
 def load_paths(project='cmip6', model='mpi-esm1-2-lr', experiment='historical', time_frequency='mon', variable='tas',
                ensemble='r1i2000p1f1', realm='atmos'):
-    paths = ['/home/joe/PycharmProjects/volai/data/input/{}/{}_ens{}.nc'.format(variable, variable, ensemble)]
-    return paths
-    #return freva.databrowser(project=project, model=model, experiment=experiment, time_frequency=time_frequency,
-    #                         variable=variable, ensemble=ensemble, realm=realm)
+    #paths = ['/home/joe/PycharmProjects/volai/data/input/{}/{}_ens{}.nc'.format(variable, variable, ensemble)]
+    #return paths
+    return freva.databrowser(project=project, model=model, experiment=experiment, time_frequency=time_frequency,
+                             variable=variable, ensemble=ensemble, realm=realm)
